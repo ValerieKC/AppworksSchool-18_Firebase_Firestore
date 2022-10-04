24a0d2a63f99f86f6879cd8b0ae9d8080371835a
Kuan-Chen Valerie Wang <valerie81.wang@gmail.com>
2022-10-04 10:32:43 +0800

    After phase1 check

--- src/component/firestore_group1.js
+++ src/component/firestore_group1.js
@@ -25,15 +25,23 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+const Container = styled.div`
+  margin: 0 auto;
+`;
+
 const Wrapper = styled.div`
-margin: 0 auto;
-padding-top:40px;
-width:1160px;
-display:flex;
-flex-direction:column;
-gap:10px;
-align-items:center;
-`
+  margin: 0 auto;
+  margin-top: 100px;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  width: 500px;
+  border: 4px solid #2c698d;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  align-items: center;
+`;
 
 const TitleInput = styled.input.attrs({
   type: "input",
@@ -94,28 +102,14 @@ function MyFireStoreApp() {
 
   async function PostData() {
     try {
-      const docRef = await addDoc(collection(db, "group1"), {
+      const docRef = await addDoc(collection(db, "Articles"), {
         title: inputTitleRef.current.value,
         content: inputContentRef.current.value,
         tag: inputTagRef.current.value,
         author_id: authorId,
         created_time: timestamp,
       });
-      // console.log("Document written with ID: ", docRef.id);
-
-      // const querySnapshot = await getDocs(collection(db, "group1"));
-      // console.log(querySnapshot);
-
-      // querySnapshot.forEach((doc) => {
-      //   console.log({
-      //     id: doc.id,
-      //     title: doc.data().title,
-      //     content: doc.data().content,
-      //     tag: doc.data().tag,
-      //     author_id: doc.data().author_id,
-      //     created_time: new Date(doc.data().created_time.seconds * 1000),
-      //   });
-      // });
+      
     } catch (e) {
       console.error("Error adding document: ", e);
     }
@@ -125,18 +119,19 @@ function MyFireStoreApp() {
     const snapshot = onSnapshot(collection(db, "group1"), (snapshot) => {
       let posts = [];
       snapshot.docs.forEach((doc) => {
-        posts.push({ ...doc.data(), id: doc.id });
+        posts.push({ ...doc.data(), id: doc.id});
+       
       });
-      posts.forEach((doc) => {
-        console.log({
-          id: doc.id,
-          title: doc.title,
-          content: doc.content,
-          tag: doc.tag,
-          author_id: doc.author_id,
-          created_time: new Date(doc.created_time.seconds * 1000),
-        });
-      });
+       posts.forEach((doc) => {
+         console.log({
+           id: doc.id,
+           title: doc.title,
+           content: doc.content,
+           tag: doc.tag,
+           author_id: doc.author_id,
+           created_time: new Date(doc.created_time.seconds * 1000),
+         });
+       });
     });
     //cleanup
     return () => {
@@ -144,50 +139,51 @@ function MyFireStoreApp() {
     };
   }, []);
 
-  //  console.log(posts);
 
   return (
-    <Wrapper>
-      <InputGroup>
-        <FormLabel>Title:</FormLabel>
-        <TitleInput ref={inputTitleRef} onChange={() => {}} />
-      </InputGroup>
-      <InputGroup>
-        <FormLabel>Tag:</FormLabel>
-        <TagInput
-          ref={inputTagRef}
-          onClick={(event) => {
-            inputTagRef.current = event.target;
-          }}
-          value={`Beauty`}
-        />
-        <Label>Beauty</Label>
-        <TagInput
-          ref={inputTagRef}
-          onClick={(event) => {
-            inputTagRef.current = event.target;
-          }}
-          value={`Gossiping`}
-        />
-        <Label>Gossiping</Label>
-        <TagInput
-          ref={inputTagRef}
-          onClick={(event) => {
-            inputTagRef.current = event.target;
-          }}
-          value={`SchoolLife`}
-        />
-        <Label>SchoolLife</Label>
-      </InputGroup>
-      <InputGroup>
-        <FormLabel>Content:</FormLabel>
-        <ContentInput ref={inputContentRef} onChange={() => {}} />
-      </InputGroup>
-      <InputGroup>
-        <FormLabel></FormLabel>
-        <SubmitBtn onClick={PostData}>Submit</SubmitBtn>
-      </InputGroup>
-    </Wrapper>
+    <Container>
+      <Wrapper>
+        <InputGroup>
+          <FormLabel>Title:</FormLabel>
+          <TitleInput ref={inputTitleRef} onChange={() => {}} />
+        </InputGroup>
+        <InputGroup>
+          <FormLabel>Tag:</FormLabel>
+          <TagInput
+            ref={inputTagRef}
+            onClick={(event) => {
+              inputTagRef.current = event.target;
+            }}
+            value={`Beauty`}
+          />
+          <Label>Beauty</Label>
+          <TagInput
+            ref={inputTagRef}
+            onClick={(event) => {
+              inputTagRef.current = event.target;
+            }}
+            value={`Gossiping`}
+          />
+          <Label>Gossiping</Label>
+          <TagInput
+            ref={inputTagRef}
+            onClick={(event) => {
+              inputTagRef.current = event.target;
+            }}
+            value={`SchoolLife`}
+          />
+          <Label>SchoolLife</Label>
+        </InputGroup>
+        <InputGroup>
+          <FormLabel>Content:</FormLabel>
+          <ContentInput ref={inputContentRef} onChange={() => {}} />
+        </InputGroup>
+        <InputGroup>
+          <FormLabel></FormLabel>
+          <SubmitBtn onClick={PostData}>Submit</SubmitBtn>
+        </InputGroup>
+      </Wrapper>
+    </Container>
   );
 }
 
